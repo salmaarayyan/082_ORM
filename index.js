4460e8af55f2c829b7d96f3f69d0a00c687c2760
salmaarayyan <salmaarayyan@gmail.com>
2025-10-27 09:53:47 +0700

    Membuat endpoint PUT untuk mengupdate data komik berdasarkan ID

--- index.js
+++ index.js
@@ -50,3 +50,21 @@ app.get('/komik', async (req, res) => {
         res.send(err);
     }
 });
+
+// Endpoint untuk mengupdate data komik berdasarkan ID
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: "Komik tidak ditemukan" });
+        }
+
+        await komik.update(data);
+        res.send({message: "Komik berhasil diupdate", komik});
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
