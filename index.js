12e41c0b04c425b91d7dfcc2bce1e0ea6b3a7caa
salmaarayyan <salmaarayyan@gmail.com>
2025-10-27 09:42:59 +0700

    perbaiki komentar dan penataan kode di index.js

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+// Import express untuk membuat server
 const express = require('express')
 const app = express();
 const PORT = 3000;
+
+// Import folder models
 const db = require("./models");
+
+// Middleware untuk membaca data JSON dan form (POST body)
 app.use(express.json());
 app.use(express.urlencoded({ 
     extended: false
 }));
 
+// Jalankan server di port 3000
 app.listen(PORT, () => {
     console.log('Server started on port 3000');
 })
 
+// Sinkronisasi model ke database (membuat tabel jika belum ada)
 db.sequelize.sync()
     .then((result) => {
+        // Jika sukses, jalankan server lagi (sebenarnya ini double listen)
         app.listen(3000, () => {
             console.log('Server started');
         })
     })
     .catch((err) => {
+        // Jika gagal konek/sync ke database, tampilkan error
         console.log(err);
     })
